891458d7cd9bf92fa47b2ebc7e0cb0f6708ed202
Connie Xie <Mingsheng@my-acer-pc.(none)>
2013-09-07 16:10:31 -0400

    request query through popup works

--- chrome_plugin/popup.js
+++ chrome_plugin/popup.js
@@ -2,6 +2,22 @@ var tok;
 document.addEventListener('DOMContentLoaded', function(){
     chrome.identity.getAuthToken({ 'interactive': true }, function(token) {
         tok = JSON.stringify(token);
+		//document.getElementById("result_box").innerHTML="syntax correct";
         console.log(tok);
+		var getURL = "https://vidium-raytray.rhcloud.com/api/retrieve/?token="+tok;
+		var xmlHttp = null;
+		xmlHttp = new XMLHttpRequest();
+		
+		xmlHttp.onreadystatechange = function (readyStateEvent) {
+		  //if (xmlHttp.readyState == 4) {
+			//if (xmlHttp.status == 200) {
+			document.getElementById("result_box").innerHTML=xmlHttp.responseText;
+			//}
+		  //}
+		}
+		xmlHttp.open( "GET", getURL, true );
+		xmlHttp.send( null );
+	   
     });
+
 });
