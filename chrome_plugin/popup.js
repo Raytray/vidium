af1b873d7838ac3a8df4178f61ab9801e7882b19
Lichard <ryli721@gmail.com>
2013-09-07 15:38:06 -0700

    made images shw in table

--- chrome_plugin/popup.js
+++ chrome_plugin/popup.js
@@ -2,22 +2,24 @@ var tok;
 document.addEventListener('DOMContentLoaded', function(){
     chrome.identity.getAuthToken({ 'interactive': true }, function(token) {
         tok = JSON.stringify(token);
-		//document.getElementById("result_box").innerHTML="syntax correct";
+        //document.getElementById("result_box").innerHTML="syntax correct";
         console.log(tok);
-		var getURL = "https://vidium-raytray.rhcloud.com/api/retrieve/?token="+tok;
-		var xmlHttp = null;
-		xmlHttp = new XMLHttpRequest();
-		
-		xmlHttp.onreadystatechange = function (readyStateEvent) {
-		  //if (xmlHttp.readyState == 4) {
-			//if (xmlHttp.status == 200) {
-			document.getElementById("result_box").innerHTML=xmlHttp.responseText;
-			//}
-		  //}
-		}
-		xmlHttp.open( "GET", getURL, true );
-		xmlHttp.send( null );
-	   
+        var getURL = "https://vidium-raytray.rhcloud.com/api/retrieve/?token="+tok;
+        var xmlHttp = null;
+        xmlHttp = new XMLHttpRequest();
+
+        xmlHttp.onreadystatechange = function (readyStateEvent) {
+            var res = JSON.parse(xmlHttp.responseText);
+            document.getElementById("result_box").innerHTML='<table>';
+
+            for(var i=0; i<res.vids.length; i++){
+                document.getElementById("result_box").innerHTML+='<tr><td><a href="' + res.vids[i].url + '"><img src="' + res.vids[i].thumb_url + '" width="320"></a></td></tr>';
+            }
+            document.getElementById("result_box").innerHTML+='</table>';
+        }
+        xmlHttp.open( "GET", getURL, true );
+        xmlHttp.send( null );
+
     });
 
 });
