079c5b20630d58fbaaa108b743f787807dc96b0e
Lichard <ryli721@gmail.com>
2013-09-07 10:47:48 -0700

    fixed popup spelling error)

--- chrome_plugin/popup.js
+++ chrome_plugin/popup.js
@@ -1,7 +1,7 @@
 var tok;
 document.addEventListener('DOMContentLoaded', function(){
     chrome.identity.getAuthToken({ 'interactive': true }, function(token) {
-        tok = toke
+        tok = JSON.stringify(token);
+        console.log(tok);
     });
 });
-console.debug(tok);
