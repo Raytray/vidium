d9e4cc8fa9ac8a862db7d15aee7548c27046b2a5
Connie Xie <Mingsheng@my-acer-pc.(none)>
2013-09-07 20:05:50 -0400

    use user id to retrieve video links

--- chrome_plugin/popup.js
+++ chrome_plugin/popup.js
@@ -4,31 +4,49 @@ document.addEventListener('DOMContentLoaded', function(){
         tok = JSON.stringify(token);
         //document.getElementById("result_box").innerHTML="syntax correct";
         console.log(tok);
-        var getURL = "https://vidium-raytray.rhcloud.com/api/retrieve/?token=106598221661789754226";
-        var xmlHttp = null;
-        xmlHttp = new XMLHttpRequest();
+        
+		var userURL = "https://www.googleapis.com/oauth2/v1/tokeninfo?access_token="+tok;
+		//alert(userURL);
+		var userHttp = null;
+		userHttp = new XMLHttpRequest();
+		
+		userHttp.onreadystatechange = function () {
+			if(userHttp.readyState==4){
+				var userRes = JSON.parse(userHttp.responseText);
+				var userId = userRes.user_id;
+				//alert(userId);		
+				var getURL = "https://vidium-raytray.rhcloud.com/api/retrieve/?token="+userId;
+				//alert(getURL);
+				var xmlHttp = null;
+				xmlHttp = new XMLHttpRequest();
 
-        xmlHttp.onreadystatechange = function (readyStateEvent) {
-            var res = JSON.parse(xmlHttp.responseText);
+				xmlHttp.onreadystatechange = function () {
+					if(xmlHttp.readyState==4){
+						
+						var res = JSON.parse(xmlHttp.responseText);
 
+						if(res.vids == "User not found"){
+							document.getElementById("result_box").innerHTML = "ADD SOME LINKS!";
+						}
+						else{
+							console.log(JSON.stringify(res));
+							console.log(res.vids.length);
+							document.getElementById("result_box").innerHTML='<table>';
 
-            if(res.vids == "User not found"){
-                document.getElementById("result_box").innerHTML = "ADD SOME LINKS!";
-            }
-            else{
-                console.log(JSON.stringify(res));
-                console.log(res.vids.length);
-                document.getElementById("result_box").innerHTML='<table>';
-
-                for(var i=0; i<res.vids.length; i++){ target="_blank"
-                    document.getElementById("result_box").innerHTML+='<tr><td><a href="' + res.vids[i].url + '" target="_blank"><img src="' + res.vids[i].thumb_url + '" width="300"></a></td></tr>';
-                }
-                document.getElementById("result_box").innerHTML+='</table>';
-            }
-        }
-        xmlHttp.open( "GET", getURL, true );
-        xmlHttp.send( null );
-
+							for(var i=0; i<res.vids.length; i++){ target="_blank"
+								document.getElementById("result_box").innerHTML+='<tr><td><a href="' + res.vids[i].url + '" target="_blank"><img src="' + res.vids[i].thumb_url + '" width="300"></a></td></tr>';
+							}
+							document.getElementById("result_box").innerHTML+='</table>';
+						}
+					}
+				}
+				xmlHttp.open( "GET", getURL, true );
+				xmlHttp.send( null );
+			}
+		}
+		userHttp.open( "GET", userURL, true );
+		userHttp.send( null );
+			
     });
 
 });
